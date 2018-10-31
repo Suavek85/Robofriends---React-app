a0313c3e28c898977c37053519706c3f5fbab526
SNiemiec <slawekniemiec@gmail.com>
2018-10-31 06:02:13 +0100

    Updates

---
+++ precache-manifest.62879e23106d9406f83fa0b526ec5af5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends---React-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d27d2585158cae58702f",
+    "url": "/Robofriends---React-app/static/js/runtime~main.d27d2585.js"
+  },
+  {
+    "revision": "0eeb24ffbc76bf3e83f8",
+    "url": "/Robofriends---React-app/static/js/main.0eeb24ff.chunk.js"
+  },
+  {
+    "revision": "ba939d8a50dd8b479e2a",
+    "url": "/Robofriends---React-app/static/js/1.ba939d8a.chunk.js"
+  },
+  {
+    "revision": "0eeb24ffbc76bf3e83f8",
+    "url": "/Robofriends---React-app/static/css/main.08ebf609.chunk.css"
+  },
+  {
+    "revision": "ba939d8a50dd8b479e2a",
+    "url": "/Robofriends---React-app/static/css/1.37df1a70.chunk.css"
+  },
+  {
+    "revision": "bb778778aa0c79342ad77bb1aee3d3dd",
+    "url": "/Robofriends---React-app/index.html"
+  }
+];
